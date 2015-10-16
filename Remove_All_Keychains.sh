684e8c30d8e56bdd71a8cd3af06caad5fcba6d89
Andrew Wilde <andrew@livingalivemedia.com>
2015-10-16 09:42:12 -0700

    Initial Upload
    Them scripts should be on Github. So I put them scripts on Github.
      Soon™

---
+++ Remove_All_Keychains.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+rm -rF /Users/$USER/Library/Keychains/.* /Users/$USER/Library/Keychains/*
---
+++ setEndPointPopupOff.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo defaults write /Library/LaunchAgents/com.checkpoint.eps.gui.plist RunAtLoad -bool false
