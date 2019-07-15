2156bd082bc00c7b1d795f2699be6889c7870f80
Andrew <andrew@livingalivemedia.com>
2019-07-15 13:29:37 -0700

    Change to install to user fonts
    System install had permission issues

--- MDM_font_install_Template.sh
+++ MDM_font_install_Template.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd /Library/Fonts
+#Will run all commands as the user
+sudo -u $(stat -f "%Su" /dev/console) /bin/sh <<'END'
+cd /Users/$USER/Library/Fonts
 curl -o font_archive.bz2 'Download_URL_here'
-sleep 3
+sleep 1.5
 mkdir OrgName_Fonts
 tar -jxvf font_archive.bz2 OrgName_Fonts
 sleep 1
 rm font_archive.bz2
+echo Fonts Installed under "/Users/$USER/Library/Fonts"
