7dfbc39758daa9dc71691ead54a381f0a13756be
Andrew Wilde <arealperson@livingalivemedia.com>
2021-02-03 11:35:46 -0800

    Create Remove-All-Daylite-Data.sh

---
+++ MDM Scripts/Remove-All-Daylite-Data.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Run as root
+#Newer versions of macOS may require local full disk permissions before this will work. You can likely add this as a TCC policy to grant your MDMs full disk access.
+#Checks to see if each file/directory/set of directories actually exists before attempting to remove them so there's not ugly error messages
+
+for LIBDIR in /Users/*/ ; do
+
+	#Removes Daylite files
+		#GroupContainers
+		[ -e "$LIBDIR"Library/"Group Containers"/GR26KTJYTV.com.marketcircle.daylite ] && rm -r "$LIBDIR"Library/"Group Containers"/GR26KTJYTV.com.marketcircle.daylite
+
+		#Preferences
+		[ -e "$LIBDIR"Library/Preferences/GR26KTJYTV.com.marketcircle.Daylite-Helper.plist ] && rm "$LIBDIR"Library/Preferences/GR26KTJYTV.com.marketcircle.Daylite-Helper.plist
+		[ -e "$LIBDIR"Library/Preferences/com.marketcircle.daylite.plist ] && rm "$LIBDIR"Library/Preferences/com.marketcircle.daylite.plist
+		[ -e "$LIBDIR"Library/Preferences/com.marketcircle.Daylite-Helper.plist ] && rm "$LIBDIR"Library/Preferences/com.marketcircle.Daylite-Helper.plist
+
+		#Caches
+		[ -e "$LIBDIR"Library/Caches/com.marketcircle.Daylite-Helper ] && rm -r "$LIBDIR"Library/Caches/com.marketcircle.Daylite-Helper
+		[ -e "$LIBDIR"Library/Caches/com.marketcircle.daylite ] && rm -r "$LIBDIR"Library/Caches/com.marketcircle.daylite
+
+		#ApplicationSupport
+		[ -e "$LIBDIR"Library/"Application Support"/com.marketcircle.Daylite-Helper/* ] && rm -r "$LIBDIR"Library/"Application Support"/com.marketcircle.Daylite-Helper/*
+		[ -e "$LIBDIR"Library/"Application Support"/com.marketcircle.daylite/* ] && rm -r "$LIBDIR"Library/"Application Support"/com.marketcircle.daylite/*
+
+
+done
+		#RemoveApp
+    rm -R /Applications/Daylite.app
+echo "Remove Complete"
